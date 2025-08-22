cb76fb1b165710a801265b22697eb6b59ae92204
Hoang Anh Tuan <hoanganhtuan95ptit@gmail.com>
2025-08-22 21:57:15 +0700

    fix build

--- app/src/main/java/com/hoanganhtuan95ptit/example/MainActivity.kt
+++ app/src/main/java/com/hoanganhtuan95ptit/example/MainActivity.kt
@@ -2,7 +2,7 @@ package com.hoanganhtuan95ptit.example
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.hoanganhtuan95ptit.autobind.reloadService
+import com.hoanganhtuan95ptit.autobind.AutoBind
 import com.hoanganhtuan95ptit.library.PaymentService
 
 class MainActivity : AppCompatActivity() {
@@ -11,6 +11,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        reloadService(PaymentService::class.java).map { it.print() }
+        AutoBind.init(application)
+        AutoBind.load(PaymentService::class.java).map { it.print() }
     }
 }
--- autobind/src/main/java/com/hoanganhtuan95ptit/autobind/AutoBind.kt
+++ autobind/src/main/java/com/hoanganhtuan95ptit/autobind/AutoBind.kt
@@ -1,5 +1,6 @@
 package com.hoanganhtuan95ptit.autobind
 
+import android.app.Application
 import android.content.Context
 import com.google.gson.Gson
 
@@ -12,50 +13,73 @@ internal data class BindingsWrapper(
     val bindings: List<Binding>
 )
 
-internal fun <T> String.createObject(clazz: Class<T>) = runCatching {
+object AutoBind {
 
-    val instance = Class.forName(this).getDeclaredConstructor().newInstance()
-    if (clazz.isInstance(instance)) clazz.cast(instance) else null
-}.getOrElse {
+    lateinit var application: Application
 
-    null
-}
 
-internal fun Context.reloadBinding() = runCatching {
+    private val allBindings = mutableListOf<Binding>()
 
-    val gson = Gson()
-
-    val allBindings = mutableListOf<Binding>()
-
-    val fileNames = assets.list("autobind") ?: emptyArray()
-
-    for (fileName in fileNames) if (fileName.endsWith(".json")) {
-
-        // Mở file JSON từ assets và đọc toàn bộ nội dung
-        val json = assets.open("autobind/$fileName")
-            .bufferedReader()
-            .use { it.readText() }
-
-        // Parse JSON thành đối tượng BindingsWrapper
-        val wrapper = gson.fromJson(json, BindingsWrapper::class.java)
-
-        // Thêm tất cả bindings vào danh sách
-        allBindings.addAll(wrapper.bindings)
+    fun init(application: Application) {
+        this.application = application
     }
 
-    allBindings
-}.getOrElse {
+    fun <T> load(clazz: Class<T>, userCache: Boolean = true): List<T> {
 
-    emptyList()
-}
+        val list = allBindings.filter {
+            clazz.name == it.type
+        }.mapNotNull {
+            it.impl.createObject(clazz = clazz)
+        }
 
-fun <T> Context.reloadService(clazz: Class<T>): List<T> {
+        if (list.isNotEmpty() && userCache) {
+            return list
+        }
 
-    val allBindings = reloadBinding()
+        allBindings.addAll(application.reloadBinding())
 
-    return allBindings.filter {
-        clazz.name == it.type
-    }.mapNotNull {
-        it.impl.createObject(clazz = clazz)
+        return allBindings.filter {
+            clazz.name == it.type
+        }.mapNotNull {
+            it.impl.createObject(clazz = clazz)
+        }
+    }
+
+
+    internal fun <T> String.createObject(clazz: Class<T>) = runCatching {
+
+        val instance = Class.forName(this).getDeclaredConstructor().newInstance()
+        if (clazz.isInstance(instance)) clazz.cast(instance) else null
+    }.getOrElse {
+
+        null
+    }
+
+    internal fun Context.reloadBinding() = runCatching {
+
+        val gson = Gson()
+
+        val allBindings = mutableListOf<Binding>()
+
+        val fileNames = assets.list("autobind") ?: emptyArray()
+
+        for (fileName in fileNames) if (fileName.endsWith(".json")) {
+
+            // Mở file JSON từ assets và đọc toàn bộ nội dung
+            val json = assets.open("autobind/$fileName")
+                .bufferedReader()
+                .use { it.readText() }
+
+            // Parse JSON thành đối tượng BindingsWrapper
+            val wrapper = gson.fromJson(json, BindingsWrapper::class.java)
+
+            // Thêm tất cả bindings vào danh sách
+            allBindings.addAll(wrapper.bindings)
+        }
+
+        allBindings
+    }.getOrElse {
+
+        emptyList()
     }
 }
