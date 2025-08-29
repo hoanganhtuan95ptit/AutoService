b3268e8cf9bbf61b68b5d2576da150c15787b1fd
Hoang Anh Tuan <hoanganhtuan95ptit@gmail.com>
2025-08-29 22:39:54 +0700

    optimize

--- autobind/src/main/java/com/hoanganhtuan95ptit/autobind/utils/exts/String.kt
+++ autobind/src/main/java/com/hoanganhtuan95ptit/autobind/utils/exts/String.kt
@@ -1,15 +1,6 @@
 package com.hoanganhtuan95ptit.autobind.utils.exts
 
-
-internal fun String.createClass() = runCatching {
-
-    Class.forName(this)
-}.getOrElse {
-
-    null
-}
-
-internal fun <T> String.createObject(clazz: Class<T>) = runCatching {
+fun <T> String.createObject(clazz: Class<T>) = runCatching {
 
     val instance = Class.forName(this).getDeclaredConstructor().newInstance()
     if (clazz.isInstance(instance)) clazz.cast(instance) else null
